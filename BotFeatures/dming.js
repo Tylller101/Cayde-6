54adb4eea0167bdddfa48e0216650076ee71b13b
tylller101 <tylller101@yahoo.com>
2021-05-10 13:18:07 -0700

    added alot to dming and added 2 features getstats and use animated emojies

--- BotFeatures/dming.js
+++ BotFeatures/dming.js
@@ -19,11 +19,37 @@ module.exports = {
                 return;
             }
 
+            if(greeting === "server"){
+                reply.send("**I am currently programed to asist in the following on the CSE & Gameing server:**\n"
+                    + "Embeding class and gaming role choice messages.\n" 
+                    + "Tracking and awarding XP, Levels and Game Tokens for participating on the server.\n"
+                    + "Sending animated emojis for users that dont have nitro.\n\n"
+                    
+                    + "**I am also programmed to conduct the following in DMs:**\n" 
+                    + "Facilitate PvP, PvE or PvPvE games(if you dont know what those mean just type it in the chat)\n"
+                    + "Inform users of there server stats or game stats(if applicable)");
+                    return;
+            }
+            if(greeting === "games"){
+                reply.send("**I am currently programed to play or facilitate the following games:**\n"
+                    + "**Dice Games:**\n"
+
+                    + "**Card Games:**\n"
+                    
+                    + "**Text RPG Games:**\n");
+                return;
+            }
+            if(greeting === "stats"){
+
+            }
             if(greeting === "help"){
-                reply.send("");
+                reply.send("If you want to know what I contribute to the server just type \"server\"\n" 
+                    + "If you want to play a game just type \"games\"\n" 
+                    + "If you would like to view your stats for the CSE & Gaming server type \"stats\"");
+                    return;
             }
             else{
-                msg.author.send("Sorry i do not know that command try using \"help\" ");
+                msg.author.send("Sorry i do not know that command check your spelling or try using \"help\" ");
             }
         }
     }
